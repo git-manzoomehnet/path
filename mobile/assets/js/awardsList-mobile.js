7808457c63b346a0d94881c8bd044788273571d8
Zeinab ansarian <zeinabansarian6280@gmail.com>
2025-05-21 16:46:13 +0330

    last

--- mobile/assets/js/awardsList-mobile.js
+++ mobile/assets/js/awardsList-mobile.js
@@ -50,3 +50,34 @@ awardBox.forEach((element,i) => {
     
     elements.forEach(el => observer.observe(el));
   }
+
+
+
+  document.querySelectorAll('.awardBox').forEach(box => {
+    box.addEventListener('click', function (e) {
+      // اگر باکس از قبل لمس شده بود، بگذار برود به لینک
+      if (this.classList.contains('touched')) return;
+
+      // جلوگیری از رفتن به لینک
+      e.preventDefault();
+
+      // اضافه کردن کلاس برای مشخص کردن لمس اول
+      this.classList.add('touched');
+
+      // نمایش عکس دوم
+      this.querySelector('.hover-img').style.opacity = '1';
+      this.querySelector('.info-box').style.opacity = '0';
+
+      // در صورت کلیک بیرون، حذف حالت
+      document.addEventListener('click', function handler(ev) {
+        if (!box.contains(ev.target)) {
+          box.classList.remove('touched');
+          box.querySelector('.hover-img').style.opacity = '0';
+          box.querySelector('.info-box').style.opacity = '1';
+          document.removeEventListener('click', handler);
+        }
+      });
+    });
+  });
+
+
